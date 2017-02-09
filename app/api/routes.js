27ffd9b096a8726bc2aca96e86cd0b82eecb0f8b
KyleMartin95 <kylemartin95@gmail.com>
2017-02-09 14:00:20 -0500

    initial website structure

---
+++ app/api/routes.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+router
+  .route('/')
+  .get(function(req, res){
+    res.render('index', {title: 'BetaPod', condition: false, css: ['index.css']})
+  });
+
+router
+  .route('/login')
+  .get(function(req, res){
+    res.render('login', {title: 'Login to BetaPod', condition: false, css: ['login.css']})
+  });
+
+router
+  .route('/help')
+  .get(function(req, res){
+    res.render('help', {title: 'BetaPod Help', condition: false})
+  });
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,34 @@
+//Bring in dependencies================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var hbs = require('express-handlebars');
+var path = require('path');
+
+// configuration =======================================
+var app = express(); //Express
+//require('./app/dataConfig/db'); // connect to db
+
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());//body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+// routes ==============================================
+app.use(express.static(__dirname + '/public')); //set route for static files
+var routes = require('./app/api/routes');
+app.use('/', routes); // configure our routes
+
+// set port and start app on 8080======================
+var port = process.env.PORT || 8080;
+app.listen(port);
+
+// shoutout to the user
+console.log('Listening to port: ' + port);
+
+// expose app
+exports = module.exports = app;
